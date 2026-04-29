d6291580728f452b99b4dd33c079b13d0a523edc
PujaGtechiemaya <puja.g@techiemaya.com>
2026-04-29 22:09:28 +0530

    updated code

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
   return (
     <main className="max-w-5xl mx-auto p-4 sm:p-6 min-h-screen">
       <Header activeSection={activeSection} onSectionClick={setActiveSection} />
-      <ExpenseChartsDashboard  />
+     { activeSection=== null && <ExpenseChartsDashboard  />}
       {activeSection === "add" && <ExpenseForm />}
       {activeSection === "summary" && (
         <>
